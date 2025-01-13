4163e2c14e17cf81ccdc85ffe79e1216db06e052
ayman-gassi <itsmeaymane2003@gmail.com>
2025-01-13 02:42:47 +0100

    Updated configuration for Render deployment

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for djangoProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
+
+# Debugging: Log the PORT environment variable
+port = os.getenv('PORT', '8000')  # Default to 8000 if PORT is not set
+print(f"WSGI is binding to PORT: {port}")
 
 application = get_wsgi_application()
